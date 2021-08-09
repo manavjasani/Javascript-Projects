4e948ac1aa5c2863dab02a8a103f70da4ecbcc84
Manav Jasani <manavjasani12@gmail.com>
2021-08-09 15:09:52 +0530

    Fix dynamic view

--- Event Countdown/script.js
+++ Event Countdown/script.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('.start-btn');
+const resetBtn = document.querySelector('.reset-btn');
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
 
+startBtn.addEventListener('click', () => {
+    setInterval(() => {
+        timer();    
+    }, 1000);
+    
+})
+
+resetBtn.addEventListener('click', () => {
+    hour.value = 0;
+    minute.value = 0;
+    second.value = 0;
+})
+
+function timer() {
+    if(second.value != 0) {
+        second.value--;
+    } else if(minute.value != 0 && second.value == 0) {
+        second.value = 59;
+        minute.value--;
+    } else if(hour.value != 0 && minute.value == 0 && second.value == 0) {
+        second.value = minute.value = 59;
+        hour.value--;
+    }
+}

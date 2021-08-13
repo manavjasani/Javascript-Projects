002b73730c466ba00aa4a42523c1c13f75bb2187
Manav Jasani <manavjasani12@gmail.com>
2021-08-13 14:53:11 +0530

    Fix energetic view of calculator

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -1,2 +1,25 @@
+const inputText = document.querySelector('.input-text');
+const tipInput = document.getElementById('tip-input');
+const tipText = document.querySelector('.tip-text');
+const container = document.querySelector('.container');
 
 
+tipInput.addEventListener('change', () => {
+    tipText.value = tipInput.value;
+    let tip = (tipText.value * inputText.value) / 100;
+    let totalBill = +inputText.value + +tip;
+
+    const div = document.createElement('div');
+    div.classList.add('result');
+    div.innerHTML = `
+            <div class="tip-amount">
+                <span class="tip-label">Tip amount : </span>
+                <span class="f-tip_amount">${tip}</span>
+            </div>
+            <div class="final-amount">
+                <span class="final-label">Total Bill with Tip : </span>
+                <span class="final_amount">${totalBill}</span>
+            </div>
+    `;
+    container.appendChild(div);
+})
